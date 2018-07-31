f75bd347ae19b506c04d027c431a6adc9598e8bf
Ulrich Petri <ulo@ulo.pe>
2018-07-31 16:58:04 +0200

    Upgrade to v0.4.1

--- raiden.rb
+++ raiden.rb
@@ -1,15 +1,15 @@
 class Raiden < Formula
   desc "Raiden Network"
   homepage "https://github.com/raiden-network/homebrew-raiden"
-  url "https://github.com/raiden-network/raiden/releases/download/v0.4.0/raiden-v0.4.0-macOS.zip"
-  version "v0.4.0"
-  sha256 "8728732aec2ac9b1a5885bf83866b2484c6d3cc1845950283fe3c08c196777b4"
+  url "https://github.com/raiden-network/raiden/releases/download/v0.4.1/raiden-v0.4.1-macOS.zip"
+  version "v0.4.1"
+  sha256 "f16e3b29e1996ccd59af498a2a951f9720a5fc2a70e7b02f6c9d9ee24886e8a2"
 
   depends_on :macos => :el_capitan
 
   def install
     # Homebrew extracts the file after download but keeps the extension - WTF?
-    mv "raiden-v0.4.0-macOS.zip", "raiden"
+    mv "raiden-v0.4.1-macOS.zip", "raiden"
     bin.install "raiden"
   end
 
